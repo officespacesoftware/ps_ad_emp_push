263cae96527b3dcf72edc97db5580dbd0baf5e33
ashsabet <ashkon@officespacesoftware.com>
2017-09-06 15:33:47 -0600

    Update push_photos_to_officespace.ps1

--- push_photos_to_officespace.ps1
+++ push_photos_to_officespace.ps1
@@ -6,12 +6,28 @@ $headers       = @{Authorization = "Token token="+$token}
 $OUDN          = "OU=Ash,DC=fs,DC=officespacesoftware,DC=com"
 $filter        = "(objectClass=user)"
 
-$ad_attributes = "SAMAccountName","email", "jpegPhoto", "thumbnailPhoto"
+$ad_Photo_Attribute = "thumbnailPhoto";
+$ad_Id_Attribute = "email";
+
+$ad_attributes = "sAMAccountName","email", $ad_Photo_Attribute
+
+$apiGetEmployees = "/api/1/employees"
+$apiPutEmployees = "/api/1/employees/"
 
 #importing module with all cmdlets
 Import-Module ActiveDirectory
 
+#prepare an MD5 converter
+$md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider;
+
 Write-Host "Getting records from OfficeSpace"
+#Get all the active employees from OfficeSpace
+$employees = (Invoke-RestMethod -Uri $baseURL$apiGetEmployees -Method get -Headers $headers).response;
+$employeesMap = @{};
+ForEach($employee in $employees) {
+    $employeesMap.add($employee.email, $employee);
+}
+Write-Output "Found $($employees.Count) EMPLOYEES"
 
 Write-Host "Getting records from AD"
 
@@ -20,85 +36,50 @@ $ADusersinOU = $OUDNsSplit[0..($OUDNsSplit.count-2)] | foreach {Get-ADUser -LDAP
 
 Write-Host "Updating OfficeSpace"
 # for each record from AD
-#  compare AD.md5 with OSS.md5
-#  if they are different then update OSS
-Write-Host "Process completed."
-
-
-do
-{
-$Array = new-object system.collections.arraylist
-foreach ($ADuserinOU in $ADusersinOU[$start..$end])
+#  compare AD.md5 with OSS.md5 image_source_fingerprint
+#  if they are different then update OSS 
+foreach ($ADuser in $ADusersinOU) {
+    #if this employee is in OfficeSpace
+    if ($employeeMap.get_item(($ADuser).$empIdAttribute))
     {
-    $imageData = ""
-    if (($ADuserinOU).$Photo.value) {
-      $imageData = [System.Convert]::ToBase64String(($ADuserinOU).$Photo.value)
-    }
-    elseif (($ADuserinOU).$Photo) {
-      $imageData = [System.Convert]::ToBase64String(($ADuserinOU).$Photo)
-    }
+        #get the OfficeSpace employee object from the map
+        $employee = $employeeMap.get_item(($ADuser).$empIdAttribute);
+        $empId = ($employee).id;
 
-$Array.Add([PSCustomObject]@{
-    "EmployeeId"= "$(($ADuserinOU).$EmployeeId)"
-    "Source"= $source
-    "FirstName"= "$(($ADuserinOU).$FirstName)"
-    "LastName"= "$(($ADuserinOU).$LastName)"
-    "Title"= "$(($ADuserinOU).$Title)"
-    "WorkPhone"= "$(($ADuserinOU).$WorkPhone)"
-    "Extension"= "$(($ADuserinOU).$Extension)"
-    "ImageData"= $imageData
-    "Department"= "$(($ADuserinOU).$Department)"
-    "Bio"= "$(($ADuserinOU).$Bio)"
-    "Email"= "$(($ADuserinOU).$Email)"
-    "StartDate"= "$(($ADuserinOU).$StartDate)"
-    "EndDate"= "$(($ADuserinOU).$EndDate)"
-    "ShowInVd"= "$(($ADuserinOU).$ShowInVd)"
-    "Udf0"= "$(($ADuserinOU).$Udf0)"
-    "Udf1"= "$(($ADuserinOU).$Udf1)"
-    "Udf2"= "$(($ADuserinOU).$Udf2)"
-    "Udf3"= "$(($ADuserinOU).$Udf3)"
-    "Udf4"= "$(($ADuserinOU).$Udf4)"
-    "Udf5"= "$(($ADuserinOU).$Udf5)"
-    "Udf6"= "$(($ADuserinOU).$Udf6)"
-    "Udf7"= "$(($ADuserinOU).$Udf7)"
-    "Udf8"= "$(($ADuserinOU).$Udf8)"
-    "Udf9"= "$(($ADuserinOU).$Udf9)"
-    "Udf10"= "$(($ADuserinOU).$Udf10)"
-    "Udf11"= "$(($ADuserinOU).$Udf11)"
-    "Udf12"= "$(($ADuserinOU).$Udf12)"
-    "Udf13"= "$(($ADuserinOU).$Udf13)"
-    "Udf14"= "$(($ADuserinOU).$Udf14)"
-    "Udf15"= "$(($ADuserinOU).$Udf15)"
-    "Udf16"= "$(($ADuserinOU).$Udf16)"
-    "Udf17"= "$(($ADuserinOU).$Udf17)"
-    "Udf18"= "$(($ADuserinOU).$Udf18)"
-    "Udf19"= "$(($ADuserinOU).$Udf19)"
-    "Udf20"= "$(($ADuserinOU).$Udf20)"
-    "Udf21"= "$(($ADuserinOU).$Udf21)"
-    "Udf22"= "$(($ADuserinOU).$Udf22)"
-    "Udf23"= "$(($ADuserinOU).$Udf23)"
-    "Udf24"= "$(($ADuserinOU).$Udf24)"}) | Out-Null
-    }
+        #get the image data from AD
+        $imageDataRaw = "";
+        if (($ADuser).$empPhotoAttribute.value) {
+            $imageDataRaw = ($ADuser).$empPhotoAttribute.value;
+        }
+        elseif (($ADuser).$empPhotoAttribute) {
+            $imageDataRaw = ($ADuser).$empPhotoAttribute;
+        }
 
-  $JSONArray = $array | ConvertTo-Json
-  $JSONArrayUTF8 = [System.Text.Encoding]::UTF8.GetBytes($JSONArray)
+        #if we found image data
+        if ($imageDataRaw -ne ""){
 
-  try {
-      Invoke-WebRequest -Uri $employee_batch_url -ContentType 'application/json; charset=utf-8' -Method Post -Body $JSONArrayUTF8 -Headers $headers -WarningAction SilentlyContinue -ErrorAction SilentlyContinue | Out-Null
-      $startn = $start+1;$endn = $end+1; Write-Host "$startn-$endn " -NoNewline; Write-Host "Done" -ForegroundColor Green
-      }
-  catch
-      {
-      $startn = $start+1;$endn = $end+1; Write-Host "$startn-$endn " -NoNewline; Write-Host $_.Exception.Message -ForegroundColor Red
-      }
+            #calculate md5 of photo in AD
+            $md5hash = [System.BitConverter]::ToString($md5.ComputeHash($imageDataRaw))
+            $md5hash = $md5hash -replace '-',''
 
-  $start+=$batchSize
-  $end+=$batchSize
-  $batch++
+            #if md5 of photo in AD does not match md5 stored in OfficeSpace
+            if ($md5hash -ine $employee.image_source_fingerprint){
+                $imageData = [System.Convert]::ToBase64String($imageDataRaw);
+
+                $request = @{
+                    record = @{
+                        imageData = $imageData
+                        }
+                    }
+
+                $JSONrequest = $request | ConvertTo-Json
+
+                Write-Host "Updating photo for user: " $employee.client_employee_id;
+                Invoke-WebRequest -Uri $baseURL$apiPutEmployees$empId -ContentType 'application/json; charset=utf-8' -Method PUT -Body $JSONrequest -Headers $headers -WarningAction SilentlyContinue -ErrorAction SilentlyContinue | Out-Null
+
+            } #end of if the md5 hashes did not match
+
+        } #end of if we found image data
 }
-while ($batch -le $batches)
 
-Write-Host "Triggering migration"
-$import_url_post_body = "Source=" + $source
-Invoke-WebRequest -Uri $employee_import_url -Method Post -Body $import_url_post_body -Headers $headers | Out-Null
-Write-Host "Completed"
+Write-Host "Process completed."
