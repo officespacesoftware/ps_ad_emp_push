97b4c69a4e54344329c7ac69e98f5260dc87a1b1
ashsabet <ashkon@officespacesoftware.com>
2017-09-08 11:34:02 -0600

    Update push_photos_to_officespace.ps1

--- push_photos_to_officespace.ps1
+++ push_photos_to_officespace.ps1
@@ -1,16 +1,17 @@
 $token         = ""
-$hostname      = "demo.officespacesoftware.com"
+$hostname      = "xxxxxx.officespacesoftware.com"
 $protocol      = "https://"
 $headers       = @{Authorization = "Token token="+$token}
 
-$OUDN          = "OU=Ash,DC=fs,DC=officespacesoftware,DC=com"
+$OUDN          = "OU=USA,DC=officespacesoftware,DC=com;"
 $filter        = "(objectClass=user)"
 
 $ad_Photo_Attribute = "thumbnailPhoto";
-$ad_Id_Attribute = "email";
+$ad_Id_Attribute = "mail";
 
 $ad_attributes = "sAMAccountName",$ad_Id_Attribute,$ad_Photo_Attribute
 
+$baseURL = "$protocol$hostname"
 $apiGetEmployees = "/api/1/employees"
 $apiPutEmployees = "/api/1/employees/"
 
@@ -20,41 +21,52 @@ Import-Module ActiveDirectory
 #prepare a MD5 calculator
 $md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider;
 
-Write-Host "Getting records from OfficeSpace"
+Write-Host "Getting records from OfficeSpace..."
+
 #Get all the active employees from OfficeSpace
 $employees = (Invoke-RestMethod -Uri $baseURL$apiGetEmployees -Method get -Headers $headers).response;
 $employeesMap = @{};
 ForEach($employee in $employees) {
-    $employeesMap.add($employee.email, $employee);
+    if ( -not ([string]::IsNullOrEmpty($employee.email)) ) { #in case this person from OfficeSpace doesn't have an email
+        if (-not $employeesMap.ContainsKey($employee.email)) { #in case there is more than one person with the same email
+            $employeesMap.add($employee.email, $employee);
+        }
+    }
 }
+
 Write-Output "Found $($employees.Count) EMPLOYEES"
 
-Write-Host "Getting records from AD"
+Write-Host "Getting records from AD..."
 
 $OUDNsSplit = $OUDN.Split(";")
-$ADusersinOU = $OUDNsSplit[0..($OUDNsSplit.count-2)] | foreach {Get-ADUser -LDAPFilter $filter -SearchScope SubTree -SearchBase $_ -Properties $ad_attributes}
+
+$ad_results = $OUDNsSplit[0..($OUDNsSplit.count-2)] | foreach {Get-ADUser -LDAPFilter $filter -SearchScope SubTree -SearchBase $_ -Properties $ad_attributes}
+
+[System.Object[]]$ADusersinOU = @()
+$ADusersinOU  = $ADusersinOU + $ad_results #we do it like this in case the get_aduser call returned only one record... we still need it to be an array
 
 Write-Host "Found $($ADusersinOU.Count) RECORDS"
 
-Write-Host "Updating OfficeSpace"
+Write-Host "Updating OfficeSpace..."
 # for each record from AD
 #  compare AD.md5 with OSS.md5 image_source_fingerprint
 #  if they are different then update OSS 
 foreach ($ADuser in $ADusersinOU) {
+
     #if this employee is in OfficeSpace
-    if ($employeeMap.get_item(($ADuser).$ad_Id_Attribute))
+    if ($employeesMap.get_item($ADuser.$ad_Id_Attribute))
     {
         #get the OfficeSpace employee object from the map
-        $employee = $employeeMap.get_item(($ADuser).$ad_Id_Attribute);
-        $empId = ($employee).id;
-
+        $employee = $employeesMap.get_item($ADuser.$ad_Id_Attribute);
+        $empId = $employee.id;
+        
         #get the image data from AD
         $imageDataRaw = "";
-        if (($ADuser).$ad_Photo_Attribute.value) {
-            $imageDataRaw = ($ADuser).$ad_Photo_Attribute.value;
+        if ($ADuser.$ad_Photo_Attribute.value) {
+            $imageDataRaw = $ADuser.$ad_Photo_Attribute.value;
         }
-        elseif (($ADuser).$empPhotoAttribute) {
-            $imageDataRaw = ($ADuser).$empPhotoAttribute;
+        elseif ($ADuser.$ad_Photo_Attribute) {
+            $imageDataRaw = $ADuser.$ad_Photo_Attribute;
         }
 
         #if we found image data
@@ -82,6 +94,8 @@ foreach ($ADuser in $ADusersinOU) {
             } #end of if the md5 hashes did not match
 
         } #end of if we found image data
+
+    } #end of if employee is in OfficeSpace
 }
 
 Write-Host "Process completed."
